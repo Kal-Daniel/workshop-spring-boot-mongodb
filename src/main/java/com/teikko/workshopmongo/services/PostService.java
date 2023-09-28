895be86a10a0263c3ff87ca04df8b5af42d827fd
Carlos Daniel <carlosdngfa@gmail.com>
2023-09-28 04:48:52 -0300

    Obtendo um post por id

---
+++ src/main/java/com/teikko/workshopmongo/services/PostService.java
@@ -0,0 +1,27 @@
+package com.teikko.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.teikko.workshopmongo.domain.Post;
+import com.teikko.workshopmongo.repository.PostRepository;
+import com.teikko.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public List<Post> findAll() {
+		return repo.findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
